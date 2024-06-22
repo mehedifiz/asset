3b0768b6bf68e8179a2c13aa8f2a9ee3a4b1a3ce
Mehedi Shanto <mehedi.shanto.contract@gmail.com>
2024-06-22 22:16:50 +0600

    Create footer and error page and make responsive layout

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,12 +1,23 @@
-
+import PageTitle from "../components/pageTitle/PageTitle";
 
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-      <aside>
-        <p>Copyright &copy;2024 - All right reserved by <span className="font-roboto font-bold uppercase">Asset<span className="text-primary font-lato">H</span>ub</span> Software Ltd.</p>
-      </aside>
-    </footer>
+    <div>
+      <section>
+        <PageTitle title={"Footer"}></PageTitle>
+      </section>
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <aside>
+          <p>
+            Copyright &copy;2024 - All right reserved by{" "}
+            <span className="font-roboto font-bold uppercase">
+              Asset<span className="text-primary font-lato">H</span>ub
+            </span>{" "}
+            Software Ltd.
+          </p>
+        </aside>
+      </footer>
+    </div>
   );
 };
 
